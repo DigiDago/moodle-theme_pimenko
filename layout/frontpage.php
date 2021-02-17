e658e26c332b174061c758493a3c1670c9d8658a
CrymSonir <revenu.sylvain@gmail.com>
2021-02-17 15:31:24 +0100

    Fix an issue were message drawer was not correctly init

--- layout/frontpage.php
+++ layout/frontpage.php
@@ -24,14 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-user_preference_allow_ajax_update(
-        'drawer-open-nav',
-        PARAM_ALPHA
-);
+user_preference_allow_ajax_update( 'drawer-open-nav', PARAM_ALPHA);
 require_once($CFG->libdir . '/behat/lib.php');
 
 if (isloggedin()) {
-    $navdraweropen = false;
+    $navdraweropen = (get_user_preferences('drawer-open-nav', 'true') == 'true');
 } else {
     $navdraweropen = false;
 }
@@ -42,7 +39,9 @@ if ($navdraweropen) {
 $bodyattributes = $OUTPUT->body_attributes($extraclasses);
 $blockshtml = $OUTPUT->blocks('side-pre');
 $hasblocks = strpos( $blockshtml, 'data-block=' ) !== false;
-$regionmainsettingsmenu = $OUTPUT->region_main_settings_menu();
+$buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_actions();
+// If the settings menu will be included in the header then don't add it here.
+$regionmainsettingsmenu = $buildregionmainsettings ? $OUTPUT->region_main_settings_menu() : false;
 $hasfrontpageregions = $OUTPUT->get_block_regions();
 $iscarouselenabled = $OUTPUT->is_carousel_enabled();
 $templatecontext = [
