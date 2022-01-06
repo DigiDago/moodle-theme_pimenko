9409aaa8029874d83b04837df998218b6f2654b9
CrymSonir <revenu.sylvain@gmail.com>
2022-01-06 15:56:28 +0100

    More fixes for moodle 4.0 and add some features like side block and course index

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = '2021042206';
+$plugin->version = '2021042207';
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = '2019111800';
