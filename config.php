04b3ae2a7ccda4506b5de670bf0ac3d232347651
CrymSonir <revenu.sylvain@gmail.com>
2022-05-12 14:58:41 +0200

    Fix more issues for Moodle 4.0 part.3

--- config.php
+++ config.php
@@ -107,18 +107,31 @@ $THEME->layouts = [
         'defaultregion' => 'side-pre',
         'options' => array('langmenu' => true),
     ),
-    // Server administration scripts.
-    'admin' => array(
+    // My dashboard page.
+    'mydashboard' => array(
         'file' => 'columns2.php',
         'regions' => array('side-pre'),
         'defaultregion' => 'side-pre',
+        'options' => array('nonavbar' => true, 'langmenu' => true),
     ),
-    // Main course page.
     'coursecategory' => array(
         'file' => 'columns2.php',
         'regions' => array('side-pre'),
         'defaultregion' => 'side-pre',
         'options' => array('langmenu' => true),
+    ),
+    // My courses page.
+    'mycourses' => array(
+        'file' => 'columns2.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+        'options' => array('nonavbar' => true),
+    ),
+    // Server administration scripts.
+    'admin' => array(
+        'file' => 'columns2.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
     ),
     'incourse' => array(
         'file' => 'columns2.php',
@@ -149,6 +162,12 @@ $THEME->layouts = [
             'side-pre',
             'side-post'
         ],
+        'defaultregion' => 'side-pre',
+    ),
+    // The pagelayout used for reports.
+    'report' => array(
+        'file' => 'columns2.php',
+        'regions' => array('side-pre'),
         'defaultregion' => 'side-pre',
     ),
 ];
