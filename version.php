46b4d590d40f14790ade50930a3b9ff3f15f8a7c
CrymSonir <revenu.sylvain@gmail.com>
2022-07-15 11:17:37 +0200

    Fix an issue with categories list in catalog also add a script install to auto load moockie2 course banner

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2022041229;
+$plugin->version = 2022041230;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2019111800;
