5d22f261659fc5595164c4e365e6e42b310c8b1d
CrymSonir <revenu.sylvain@gmail.com>
2024-04-02 10:03:37 +0200

    [HOTFIX] Update plugin version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2022101910;
+$plugin->version = 2022101911;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2019111800;
