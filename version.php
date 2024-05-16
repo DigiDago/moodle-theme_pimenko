ceb98d7709a9123a940645351443e7c61d812c39
Sylvain <revenu.sylvain@gmail.com>
2024-05-16 09:07:51 +0200

    Update version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2023051400;
+$plugin->version = 2023051401;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2019111800;
