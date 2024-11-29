08c4f4995becc0dffdac2d72947a83a7ea852f63
CrymSonir <revenu.sylvain@gmail.com>
2024-11-29 10:23:55 +0100

    Fix issues responsive catalog

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2023062001;
+$plugin->version = 2023062002;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2024042200;
